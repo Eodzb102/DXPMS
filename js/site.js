11c16baa785d5b7312cbd613d2fd92ef5d8a2fbf
holymoly <you@example.commotessori.iron@montessori.co.kr>
2024-04-15 15:40:39 +0900

    타임라인 드래그 기능 추가

--- js/site.js
+++ js/site.js
@@ -19,10 +19,14 @@ $(function () {
     $('.modal').removeClass('on');
   });
 
+  $('input[type="checkbox"], label').on('click', function (e) {
+    e.stopPropagation();
+  });
+
   $('.option_filter_item').on('click', function (e) {
     e.stopPropagation();
     $(this).siblings().removeClass('active');
-    $(this).addClass('active');
+    $(this).toggleClass('active');
   });
 
   $('.table_head button').on('click', function () {
@@ -42,4 +46,67 @@ $(function () {
     }
     $('.option_filter_item').removeClass('active');
   });
+
+  date_set();
+
+  function date_set() {
+    // dtae 가져오기
+    let start = $('.time_date').data()['startDate'].replace('20', '').split('-');
+    let end = $('.time_date').data()['endDate'].replace('20', '').split('-');
+    let start_date = {
+      yearMont: `${start[0]}-${start[1]}`,
+      day: start[2],
+    };
+    let end_date = {
+      yearMont: `${end[0]}-${end[1]}`,
+      day: end[2],
+    };
+
+    // console.log(start_date, end_date);
+
+    // 날짜 위치 찾기
+    // console.log($('.time_week_title'));
+    $('.time_week_title').each(function () {
+      // console.log();
+      if ($(this).data()['yearMonth'] == start_date.yearMont) {
+        // console.log('맞습니다');
+      } else {
+        // console.log('아닙니다');
+      }
+    });
+  }
+
+  drag();
+
+  function drag() {
+    let maxX = $('.work_title').outerWidth();
+    let minX = 80 + $('.width_bar').outerWidth();
+    let startX = 0;
+    let delX = 0;
+    let offsetX = 0;
+
+    $('.width_bar').css({ left: maxX - 2 + 'px' });
+
+    $('.width_bar').on('mousedown', function (e) {
+      e.preventDefault();
+      startX = e.clientX;
+      offsetX = $(this).position().left;
+
+      $(document).on('mousemove', function (e) {
+        delX = e.clientX - startX;
+        console.log($('.width_bar').position().left + $('.width_bar').outerWidth(), minX, delX);
+        if ($('.work_title').outerWidth() >= minX || delX > 0) {
+          $('.width_bar').css({ left: offsetX + delX + 'px' });
+          $('.work_title').css({ width: offsetX + delX + 1 + 'px' });
+        }
+      });
+      $(document).on('mouseup', function () {
+        if ($('.width_bar').position().left > maxX) {
+          $('.work_title').css({ width: maxX });
+          $('.width_bar').css({ left: maxX - 2 + 'px' });
+        }
+        $(document).off('mousemove mouseup');
+      });
+    });
+  }
 });
